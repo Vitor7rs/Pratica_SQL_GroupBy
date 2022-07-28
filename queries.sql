eafaee3ff1a5fb268701bec36421929e4e8056f5
Vitor Macedo <vitorjvbm@gmail.com>
2022-07-28 17:00:41 -0300

    Ex2

--- queries.sql
+++ queries.sql
@@ -2,3 +2,18 @@
 
 SELECT COUNT("endDate") FROM experiences;
 
+
+-- Exercício 2
+
+SELECT "userId" AS id, COUNT("courseId") AS educations
+FROM educations
+GROUP BY "userId"
+ORDER BY educations DESC
+
+
+-- Exercício 3
+
+
+
+
+-- Exercício 4
